0bdf210d97fa19eb7616737333ba23c990dcef07
niteshbhagat8389874345 <120049066+niteshbhagat8389874345@users.noreply.github.com>
2022-12-17 13:01:35 +0530

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -18,7 +18,7 @@ send me by t.me/i_want_to_be_isekaied *audio + command* with which you will resp
 `
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
-    conn.sendButton(m.chat, m2, '▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
+    conn.sendButton(m.chat, m2, '▢ komi  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Groups', `${usedPrefix}gpdylux`]
     ],m, rpyt)
